d47800afb0b087a8600f1373a004f981740c8fb1
luiz_ <luiz_carvalho20@hotmail.com>
2026-01-12 14:12:53 -0300

    Desafio concluído: Microsserviços Docker configurados e validados

--- dockerfile
+++ dockerfile
@@ -1,2 +1,6 @@
-FROM nginx
-COPY nginx.conf /etc/nginx/nginx.conf
+FROM php:7.4-fpm
+
+RUN docker-php-ext-install mysqli pdo pdo_mysql
+
+WORKDIR /var/www/html
+COPY . .
--- index.php
+++ index.php
@@ -1,48 +1,17 @@
-<html>
-
-<head>
-<title>Exemplo PHP</title>
-</head>
-<body>
-
 <?php
-ini_set("display_errors", 1);
-header('Content-Type: text/html; charset=iso-8859-1');
+error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
 
+echo "Versao Atual do PHP: " . phpversion() . "<br><br>";
 
+$link = new mysqli("db", "root", "senha", "meubanco");
 
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
-
-$servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
-
-// Criar conexão
-
-
-$link = new mysqli($servername, $username, $password, $database);
-
-/* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
 
-$valor_rand1 =  rand(1, 999);
-$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
-$host_name = gethostname();
+echo "Conexão com o MySQL realizada com sucesso!<br>";
+echo "Projeto Toshiro Shibakita - Infraestrutura via Docker Compose.";
 
-
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
-
-if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $link->error;
-}
-
+$link->close();
 ?>
-</body>
-</html>
